bd8fed5a70745ec6f7f660f6e409864cfbce32d0
Ansh Thakur <mailtodroidoox@gmail.com>
2025-04-11 23:55:57 +0530

    DONE

---
+++ Day2.java
@@ -0,0 +1,14 @@
+public class Day2 {
+    public static void main(String args[]) {
+        // int num;
+        double digit = 73938;
+        char check = 'A';
+        float digi = 24;
+        boolean check2 = true;
+        // System.out.println(num); //throws error
+        System.out.println(digit);
+        System.out.println(check);
+        System.out.println(check2);
+        System.out.println(digi);
+    }
+}
